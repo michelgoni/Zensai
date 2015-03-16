90f29131a10ef5cf21eef8aa50e9b246186a8e9c
Michel Goñi <michelgoni2@gmail.com>
2015-03-16 11:02:29 +0100

    Added json files for flavours & ingredients

---
+++ GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  Sabores
+//
+
+//  Copyright (c) 2015 Michel Goñi. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  Sabores
+//
+
+//  Copyright (c) 2015 Michel Goñi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
