4bd63731af3a203f11a6de26ac3c44eb131b0356
Michel Goñi <michelgoni2@gmail.com>
2015-03-17 18:30:23 +0100

    Added ingredientById method

--- Model/Ingrediente.m
+++ Model/Ingrediente.m
@@ -8,6 +8,17 @@
 
 @implementation Ingrediente
 
-// Custom logic goes here.
++ (Ingrediente *)ingredienteById:(NSString *)identifier inContext:(NSManagedObjectContext *)context {
+    
+    
+    NSArray *ingredientes = [super fetchIngredienteById:context];
+    
+    if (ingredientes.count == 0) {
+        return nil;
+    }
+    
+    return (Ingrediente *)ingredientes[0];
+    
+}
 
 @end
