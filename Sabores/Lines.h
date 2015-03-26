e04dce5b1e2ecd2cc5022457ba789f13b5401563
Michel Goñi <michelgoni2@gmail.com>
2015-03-26 01:14:24 +0100

    New class for creating hexagon lines

---
+++ Sabores/Lines.h
@@ -0,0 +1,13 @@
+//
+//  Lines.h
+//  Sabores
+//
+//  Created by Michel Goñi on 25/3/15.
+//  Copyright (c) 2015 Michel Goñi. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface Lines : SKNode
+
+@end
---
+++ Sabores/Lines.m
@@ -0,0 +1,13 @@
+//
+//  Lines.m
+//  Sabores
+//
+//  Created by Michel Goñi on 25/3/15.
+//  Copyright (c) 2015 Michel Goñi. All rights reserved.
+//
+
+#import "Lines.h"
+
+@implementation Lines
+
+@end
